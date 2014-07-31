a2363f1cde8482d8176edc7d7770b368bc56fd6b
Eric Croom <ericcroom@Eric-Crooms-MacBook-Pro.local>
2014-07-30 20:19:54 -0700

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Eric Croom on 7/30/14.
+//  Copyright (c) 2014 Eric Croom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Eric Croom on 7/30/14.
+//  Copyright (c) 2014 Eric Croom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
