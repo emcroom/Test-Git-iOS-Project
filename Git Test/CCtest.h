ec00a54285c1a7bbba2309df823ca6173e7fa5a8
Eric Croom <ericcroom@Eric-Crooms-MacBook-Pro.local>
2014-07-30 20:32:06 -0700

    added a test class

---
+++ Git Test/CCtest.h
@@ -0,0 +1,13 @@
+//
+//  CCtest.h
+//  Git Test
+//
+//  Created by Eric Croom on 7/30/14.
+//  Copyright (c) 2014 Eric Croom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCtest : NSObject
+
+@end
---
+++ Git Test/CCtest.m
@@ -0,0 +1,13 @@
+//
+//  CCtest.m
+//  Git Test
+//
+//  Created by Eric Croom on 7/30/14.
+//  Copyright (c) 2014 Eric Croom. All rights reserved.
+//
+
+#import "CCtest.h"
+
+@implementation CCtest
+
+@end
